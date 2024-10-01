19a98aeecf0f40c587118371a19a4742809de5d5
mqxs21 <maxshi239@gmail.com>
2024-09-30 20:47:11 -0700

    inital commit

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    public Transform playerTransform;
+    public bool enemyCanMove = true;
+    public int maxHp = 100;
+    public int currHp = 100;
+
+    void Start()
+    {
+        currHp = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCanMove)
+        {
+            agent.SetDestination(playerTransform.position);
+        }
+    }
+    public void hit(int damage){
+        currHp-=damage;
+    }
+}
