6189b8a5af3af8cebb21ad323d15453d0635fb6d
mqxs21 <maxshi239@gmail.com>
2024-10-01 21:10:06 -0700

    stuff

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -14,30 +15,51 @@ public class EnemyAI : MonoBehaviour
     public LayerMask whatIsPlayer;
     public Animator skeleAnimator;
     public bool playerInSightRange;
+    public bool playerInAttackRange;
+    public float attackRange = 2;
+    public bool stillAlive = true;
     void Start()
     {
+        stillAlive = true;
         currHp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (playerInSightRange)
+        while(stillAlive){
+             if (playerInSightRange && !playerInAttackRange)
         {
+            agent.enabled = true;
             agent.SetDestination(playerTransform.position);
+            Debug.Log("in sight range");
+            skeleAnimator.SetBool("isWalking",true);
+            skeleAnimator.SetBool("isAttacking",false);
+            
+        }else if (playerInAttackRange)
+        {
+            Debug.Log("in attack range");
+            agent.SetDestination(transform.position);
+            skeleAnimator.SetBool("isAttacking",true);
+            skeleAnimator.SetBool("isWalking",false);
+        }else{
+            agent.enabled = false;
+            skeleAnimator.SetBool("isAttacking",false);
+            skeleAnimator.SetBool("isWalking",false);
         }
+        
+        }
+       
         if (currHp<=0)
         {
+            stillAlive = false;
+            skeleAnimator.SetBool("isAttacking",false);
+            skeleAnimator.SetBool("isWalking",false);
             Die();
         }
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        if (playerInSightRange)
-        {
-            agent.enabled = true;
-        }else{
-            agent.enabled = false;
-        }
+        playerInAttackRange = Physics.CheckSphere(transform.position,attackRange,whatIsPlayer);
+
     }
     public void hit(int damage){
         currHp-=damage;
@@ -48,8 +70,13 @@ public class EnemyAI : MonoBehaviour
             hit(10);
         }
     }
-    void Die(){
-        Destroy(this.transform.parent.gameObject);
-    }
+
+    void Die() {
+        skeleAnimator.SetBool("isDead", true); 
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        agent.enabled = false; 
+        Destroy(gameObject, 3f); 
+}
+
    
 }
