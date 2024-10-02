e2f4524445c80e2670bcfa526503593a04e8a6be
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-10-01 21:41:58 -0700

    Food gives hunger back

--- Assets/Scripts/FarmingStuff.cs
+++ Assets/Scripts/FarmingStuff.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class FarmingStuff : MonoBehaviour
 {
+    public HungerController hungerController;
   //  public GameObject thePitchfork;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (hungerController == null)
+        {
+            hungerController = FindObjectOfType<HungerController>();
+        }
     }
     // Update is called once per frame
     void Update()
     {
     }
+
+    void IncreaseHunger(float amount)
+    {
+        if (hungerController != null)
+        {
+            hungerController.hungerPercentage += amount;
+            hungerController.hungerPercentage = Mathf.Clamp(hungerController.hungerPercentage, 0, hungerController.maxHunger);
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
 
         if (collision.gameObject.tag == "Food")
         {
+            Destroy(collision.gameObject);
+            IncreaseHunger(10f);
             Debug.Log("Collision with Food!");
         }
     }
